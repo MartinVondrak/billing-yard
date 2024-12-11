39aae11f0b7a15d6994528230576331d28cfb10e
Martin Vondrák <martinvondrak@icloud.com>
2024-12-11 10:58:53 +0100

    fixup! [#1] Fix VAT summary and add QR code with payment info

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='billingyard',
-    version='1.2.0-dev',
+    version='1.2.0',
     author='Martin Vondrák',
     author_email='martin@martinvondrak.cz',
     description='CLI invoice generator for business entities in the Czech republic',
